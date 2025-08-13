554b511706409e97ca8a4a7cb54ea534d99e0829
DEVStynx <131193709+DEVStynx@users.noreply.github.com>
2025-08-13 15:07:27 +0200

    Changed Mappings to ParchmentMC

--- common/src/main/java/de/stynxyxy/shurikens/ExampleMod.java
+++
@@ -1,9 +0,0 @@
-package de.stynxyxy.shurikens;
-
-public final class ExampleMod {
-    public static final String MOD_ID = "shurikens";
-
-    public static void init() {
-        // Write common init code here.
-    }
-}
